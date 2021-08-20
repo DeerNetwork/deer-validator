aebde9db56a9b0c87963a89526a41242ba8576d1
sigoden <sigoden@gmail.com>
2021-08-20 18:23:49 +0800

    fix mainnet

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,8 +3,10 @@
 uninstall()
 {
 	if [ -f /usr/bin/deer ]; then
-		batch_exec exec_docker_rm
-		batch_exec exec_docker_rmi
+		for item in "${service_images[@]}" ; do
+			docker rm -f "${item%%:*}"
+			docker rmi -f "${item##*:}"
+		done
 		local chain_data_dir=$(cat $installdir/config.json | jq -r '.chain_data_dir')
 		[ -d "$chain_data_dir" ] && rm -r $chain_data_dir
 		rm /usr/bin/deer
